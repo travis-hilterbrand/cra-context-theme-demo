2322dd813190f533d5a6aabd3a813e19f6eeb33b
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-07-14 22:25:39 -0700

    Simple example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 import "./App.css";
-import MyComponent from "./MyComponent";
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee",
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222",
+  },
+};
+const ThemeContext = React.createContext(themes.light);
 
+function ThemedButton() {
+  const theme = React.useContext(ThemeContext);
+  console.log("theme", theme);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
 function App() {
+  /*
+<ThemeContext.Provider value={themes.dark}>
+</ThemeContext.Provider>
+*/
   return (
     <div className="App">
-      <MyComponent />
+      <ThemedButton />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeContextProvider } from "./ThemeContext";
 
 import App from "./App";
 
-ReactDOM.render(
-  <ThemeContextProvider>
-    <App />
-  </ThemeContextProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
